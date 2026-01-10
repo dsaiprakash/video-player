34463a9bf060ab1c2d171c8a7522e019687d297b
dsaiprakash <prakashreddy4565@gmail.com>
2026-01-10 21:41:45 +0530

    plyr controls updated and download button issue fix

--- script.js
+++ script.js
@@ -193,7 +193,6 @@ document.getElementById("downloadBtn").addEventListener("click", () => {
     const link = document.createElement("a")
     link.href = currentVideoUrl
     link.download = "video"
-    link.target = "_blank"
     document.body.appendChild(link)
     link.click()
     document.body.removeChild(link)
